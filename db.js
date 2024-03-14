d8238cbb3b1c05ef86460cb14686d3ccfff52b47
Alison Moura <alison.oghino@gmail.com>
2024-03-14 12:11:24 -0400

    Adicionando comentários e JSDocs

--- db.js
+++ db.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 
+/**
+ * Função responsável por realizar a conexão com o banco de dados MongoDB
+ */
 export async function conectarBancoDeDados() {
     console.log("Vai conectar com o banco de dados...");
     await mongoose.connect("mongodb+srv://betnget:betnget_mongodb@cluster0.fljwote.mongodb.net/betnget?retryWrites=true&w=majority&appName=Cluster0");
--- index.js
+++ index.js
@@ -5,16 +5,16 @@ const PORT = 3000
 
 async function run() {
     const app = express();
-    app.use(cors());
-    app.use(express.json());
+    app.use(cors()); // habilita o CORS para consumo da API
+    app.use(express.json()); // habilita que a API leia requisições com body contendo JSON
 
-    await conectarBancoDeDados();
+    await conectarBancoDeDados(); // Estabelece a conexão com o banco de dado MongoDB (MongoDB Atlas)
 
-    app.get("/", function (req, res) {
+    app.get("/", function (req, res) { // Endpoint GET raiz
         res.send("API Betnget");
     })
 
-    app.listen(PORT, function() {
+    app.listen(PORT, function() { // Incia a API na porta 3000
         console.log("API rodando na porta: " + PORT);
     })
 }
