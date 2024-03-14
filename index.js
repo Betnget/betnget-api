b02817866fead28503ae276aef7ab9324f9a553a
Alison Moura <alison.oghino@gmail.com>
2024-03-14 13:05:40 -0400

    Criando endpoint para buscar todos os usuarios e busca de usuario por id

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import express from "express";
 import cors from "cors";
 import { conectarBancoDeDados } from './db.js';
+import mongoose from "mongoose";
 const PORT = 3000
 
+/**
+ * Função responsável por iniciar a API: 
+ * Configura o CORS
+ * Configura express.json()
+ * Estabelece a conexão com banco de dados
+ * Registra os endpoints
+ */
 async function run() {
     const app = express();
     app.use(cors()); // habilita o CORS para consumo da API
@@ -10,13 +18,26 @@ async function run() {
 
     await conectarBancoDeDados(); // Estabelece a conexão com o banco de dado MongoDB (MongoDB Atlas)
 
-    app.get("/", function (req, res) { // Endpoint GET raiz
+    app.get("/", function(req, res) { // Endpoint GET raiz
         res.send("API Betnget");
     })
 
+    app.get("/usuarios", async function(req, res) {
+        // Acessar o UsuarioModel
+        const UsuarioModel = mongoose.model("Usuario"); // mongoose retorna o modelo já registrado
+        const usuariosCadastrados = await UsuarioModel.find({}); // mongoose faz uma busca de todos os documentos na coleção 'usuarios'
+        res.json(usuariosCadastrados);
+    });
+
+    app.get("/usuarios/:id", async function(req, res){
+        const UsuarioModel = mongoose.model("Usuario");
+        const usuarioEncontrado = await UsuarioModel.findById(req.params.id);
+        res.json(usuarioEncontrado);
+    });
+
     app.listen(PORT, function() { // Incia a API na porta 3000
         console.log("API rodando na porta: " + PORT);
-    })
+    });
 }
 
 run();
